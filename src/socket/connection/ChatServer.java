853342daa576b898a8e0f6cba42e45674ee3b648
Bobizl <bobi_aytos@abv.bg>
2023-05-13 23:29:22 +0300

    Initial commit

--- src/socket/connection/ChatServer.java
+++ src/socket/connection/ChatServer.java
@@ -31,9 +31,15 @@ public class ChatServer {
     }
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Syntax: java ChatServer <port-number>");
+            System.exit(0);
+        }
         int port = Integer.parseInt(args[0]);
         ChatServer server = new ChatServer(port);
         server.execute();
     }
+
+    public void chatBroadcast(String message, User)
 
 }
