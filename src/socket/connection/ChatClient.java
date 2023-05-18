e535b01e2ce1fe57863daff03ca23708c8c9371c
Bobizl <bobi_aytos@abv.bg>
2023-05-18 14:08:07 +0300

    Initial commit

---
+++ src/socket/connection/ChatClient.java
@@ -0,0 +1,12 @@
+package socket.connection;
+
+public class ChatClient {
+    private String hostname;
+    private int port;
+    private String userName;
+
+    public ChatClient(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+}
