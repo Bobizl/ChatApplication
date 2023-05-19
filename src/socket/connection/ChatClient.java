83915fe245f1b6fdc8472a1503c022fbf3f4b8bf
Bobizl <bobi_aytos@abv.bg>
2023-05-19 14:00:55 +0300

    Initial commit

--- src/socket/connection/ChatClient.java
+++ src/socket/connection/ChatClient.java
@@ -1,4 +1,7 @@
 package socket.connection;
+import java.net.*;
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 public class ChatClient {
     private String hostname;
@@ -9,4 +12,21 @@ public class ChatClient {
         this.hostname = hostname;
         this.port = port;
     }
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+
+            System.out.println("Connected to the chat server");
+
+            new ReadThread(socket, this).start();
+            new WriteThread(socket, this).start();
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+    }
+
+
 }
